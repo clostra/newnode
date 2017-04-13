b9eb812ea1ca05c01b3460ac403652cb96ebac4a
Greg Hazel <ghazel@gmail.com>
2017-04-13 13:38:30 -0700

    Revert "no need to extern"
    This reverts commit b88cc6fdf281bbe3c80da5fffc2da38f05a35ba9.

--- sha1.h
+++ sha1.h
@@ -13,6 +13,10 @@
 
 #include "stdint.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 typedef struct {
     uint32_t state[5];
@@ -33,6 +37,10 @@ void SHA1Final(unsigned char digest[20], SHA1_CTX *context);
 #define SHA1(digest, buf, len) CC_SHA1(buf, len, digest)
 #else
 void SHA1(unsigned char *hash_out, const unsigned char *str, int len);
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif // __SHA1_H__
