f393847922f26aa797e7610fc889a6ba8bebedb9
Greg Hazel <ghazel@gmail.com>
2017-04-12 23:24:30 -0700

    put support, and cross platform sha1

--- dht.h
+++ dht.h
@@ -10,6 +10,7 @@ extern "C" {
 typedef struct dht dht;
 
 typedef void (^add_nodes_callblock)(const byte *peers, uint num_peers);
+typedef void (^put_complete_callblock)();
 
 dht* dht_setup(int fd);
 void dht_tick(dht *d);
@@ -17,6 +18,7 @@ bool dht_process_udp(dht *d, const byte *buffer, size_t len, const struct sockad
 bool dht_process_icmp(dht *d, const byte *buffer, size_t len, const struct sockaddr *to, socklen_t tolen);
 void dht_announce(dht *d, const byte *info_hash, add_nodes_callblock cb);
 void dht_get_peers(dht *d, const byte *info_hash, add_nodes_callblock cb);
+void dht_put(dht *d, const byte *pkey, const byte *skey, const char *v, int64 seq, put_complete_callblock cb);
 void dht_destroy(dht *d);
 
 #ifdef __cplusplus
