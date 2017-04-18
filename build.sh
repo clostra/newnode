54d6c74a3d7c394ee47da8d95a1286bdca61ca6d
Greg Hazel <ghazel@gmail.com>
2017-04-18 02:37:30 -0700

    use libevent for networking. add timer

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ bjam toolset=clang cxxflags="-std=c++14"
 cp `find bin -name libbtdht.a` .
 cd ..
 
-FLAGS="-O0 -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Werror=shadow -Wfatal-errors \
+FLAGS="-g -O3 -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Werror=shadow -Wfatal-errors \
   -fPIC -fblocks -fdata-sections -ffunction-sections \
   -fno-rtti -fno-exceptions -fno-common -fno-inline -fno-optimize-sibling-calls -funwind-tables -fno-omit-frame-pointer -fstack-protector-all \
   -std=gnu11 -D__FAVOR_BSD -D_BSD_SOURCE"
@@ -25,8 +25,9 @@ CPPFLAGS="$FLAGS -std=c++14"
 echo "int main() {}"|clang -x c - -lrt 2>/dev/null && LRT="-lrt"
 
 clang $CPPFLAGS -c dht.cpp -I ./libbtdht/src -I ./libbtdht/btutils/src
-clang $CFLAGS -o injector injector.c log.c icmp_handler.c network.c sha1.c dht.o \
+clang $CFLAGS -o injector injector.c log.c icmp_handler.c network.c sha1.c timer.c dht.o \
   -I ./libutp libutp/libutp.a \
   ./libbtdht/libbtdht.a ./libbtdht/btutils/libbtutils.a \
   `pkg-config --cflags libsodium` `pkg-config --libs libsodium` \
+  `pkg-config --cflags libevent` `pkg-config --libs libevent libevent_pthreads` \
   -lc++ $LRT
