9245dd65796543c85f715dddd8ab1fb2180bac79
Greg Hazel <ghazel@gmail.com>
2017-04-17 22:28:37 -0700

    fail on first error

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 cd libutp
 make
 cd ..
@@ -11,7 +14,7 @@ bjam toolset=clang cxxflags="-std=c++14"
 cp `find bin -name libbtdht.a` .
 cd ..
 
-FLAGS="-g -O3 -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Werror=shadow \
+FLAGS="-O0 -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Werror=shadow -Wfatal-errors \
   -fPIC -fblocks -fdata-sections -ffunction-sections \
   -fno-rtti -fno-exceptions -fno-common -fno-inline -fno-optimize-sibling-calls -funwind-tables -fno-omit-frame-pointer -fstack-protector-all \
   -std=gnu11 -D__FAVOR_BSD -D_BSD_SOURCE"
@@ -19,12 +22,9 @@ FLAGS="-g -O3 -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-par
 CFLAGS="$FLAGS -std=gnu11"
 CPPFLAGS="$FLAGS -std=c++14"
 
-echo "int main() {}"|$CC -x c - -lrt 2>/dev/null
-if [ $? -eq 0 ]; then
-    LRT="-lrt"
-fi
+echo "int main() {}"|clang -x c - -lrt 2>/dev/null && LRT="-lrt"
 
-clang $CPPFLAGS -c dht.cpp -I ./libbtdht/src -I ./libbtdht/btutils/src 
+clang $CPPFLAGS -c dht.cpp -I ./libbtdht/src -I ./libbtdht/btutils/src
 clang $CFLAGS -o injector injector.c log.c icmp_handler.c network.c sha1.c dht.o \
   -I ./libutp libutp/libutp.a \
   ./libbtdht/libbtdht.a ./libbtdht/btutils/libbtutils.a \
