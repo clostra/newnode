968d8f90ce6aee9cde79b5246b3b1eff973ded1c
Peter Jankuliak <p.jankuliak@gmail.com>
2017-05-01 09:27:41 +0200

    Use libsodium from submodule
    Plus minor changes to fix travis build.

--- build.sh
+++ build.sh
@@ -23,6 +23,13 @@ bjam toolset=clang cxxflags="-std=c++1y"
 cp `find bin -name libbtdht.a` .
 cd ..
 
+echo "Building libsodium..."
+cd libsodium
+if [ ! -f configure ]; then ./autogen.sh; fi
+if [ ! -f Makefile ]; then ./configure; fi
+make
+cd ..
+
 FLAGS="-g -O0 -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Wno-error=shadow -Wfatal-errors \
   -fPIC -fblocks -fdata-sections -ffunction-sections \
   -fno-rtti -fno-exceptions -fno-common -fno-inline -fno-optimize-sibling-calls -funwind-tables -fno-omit-frame-pointer -fstack-protector-all \
@@ -39,18 +46,23 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     CFLAGS="$CFLAGS -lstdc++ -lm -lBlocksRuntime -lpthread"
 fi
 
-clang $CPPFLAGS -c dht.cpp -I ./libbtdht/src -I ./libbtdht/btutils/src
+echo "Building dht.o lib..."
+clang $CPPFLAGS -c dht.cpp -I ./libbtdht/src -I ./libbtdht/btutils/src -I ./libsodium/src/libsodium/include
+
+echo "Building injector..."
 clang $CFLAGS -o injector injector.c log.c icmp_handler.c network.c sha1.c timer.c utp_bufferevent.c dht.o \
   -I ./libutp libutp/libutp.a \
   -I ./Libevent/include ./Libevent/.libs/libevent.a ./Libevent/.libs/libevent_pthreads.a \
+  -I ./libsodium/src/libsodium/include ./libsodium/src/libsodium/.libs/libsodium.a \
   ./libbtdht/libbtdht.a ./libbtdht/btutils/libbtutils.a \
-  `pkg-config --cflags libsodium` `pkg-config --libs libsodium` \
   `pkg-config --cflags libevent` \
-  -lc++ $LRT $LM $LB
+  $LRT $LM $LB
+
+echo "Building injector_helper..."
 clang $CFLAGS -o injector_helper injector_helper.c log.c icmp_handler.c network.c sha1.c timer.c utp_bufferevent.c dht.o \
   -I ./libutp libutp/libutp.a \
   -I ./Libevent/include ./Libevent/.libs/libevent.a ./Libevent/.libs/libevent_pthreads.a \
+  -I ./libsodium/src/libsodium/include ./libsodium/src/libsodium/.libs/libsodium.a \
   ./libbtdht/libbtdht.a ./libbtdht/btutils/libbtutils.a \
-  `pkg-config --cflags libsodium` `pkg-config --libs libsodium` \
   `pkg-config --cflags libevent` \
-  -lc++ $LRT $LM $LB
+  $LRT $LM $LB
