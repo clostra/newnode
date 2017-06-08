1fa2bd5ba1b7c5c18bb67401560d31fdb566b54f
Peter Jankuliak <p.jankuliak@gmail.com>
2017-06-08 11:50:27 +0200

    Test error response if injector is down

--- test.sh
+++ test.sh
@@ -6,10 +6,10 @@ if [ -z "$ASAN_OPTIONS" ]; then
     export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
 fi
 
-origin_addr=localhost
-origin_port=8080
-injector_tcp_port=8005
-proxy_tcp_port=5678
+LOCAL_ORIGIN=localhost:8080
+INJECTOR_TCP_PORT=8005
+HELPER_TCP_PORT=5678
+
 HTTP_OK=200
 HTTP_MOVED=301
 HTTP_FOUND=302
@@ -44,21 +44,24 @@ function prepend {
 }
 
 function do_curl {
-    local proxy=localhost:$1
+    local proxy=""
+    [ -n "$1" ] && proxy="-x localhost:$1"
     local host=$2
-    local code=$(curl -x $proxy $host -o /dev/null -w "%{http_code}" --silent --show-error)
-    element_in $code "${@:3}" && return 0 || return 1
+    local code=$(curl $proxy $host -o /dev/null -w "%{http_code}" --silent --show-error)
+    if ! element_in $code "${@:3}"; then
+        echo "Expected HTTP response one of (${@:3}) but received $code"
+        return 1
+    fi
+    return 0
 }
 
 function test_n {
     local n=$1
     local host=$2
-    echo "$(now) Testing with $n jobs"
-
     local pids=()
     
     for ((i=0;i<$n;i++)); do
-        do_curl $proxy_tcp_port $host "${@:3}" &
+        do_curl $HELPER_TCP_PORT $host "${@:3}" &
         pids+=("$!")
     done
     
@@ -74,25 +77,28 @@ function test_n {
     return $r
 }
 
-if [ "$origin_addr" == "localhost" ]; then
-    ./test_server & # listens on origin_port
-    server_pid=$!
-    all_jobs+=("$server_pid")
-fi
+#-------------------------------------------------------------------------------
+./test_server &
+server_pid=$!
+all_jobs+=("$server_pid")
 
 echo "$(now) Starting injector."
 $unbuf ./injector -p 7000 2> >(prepend "Ie") 1> >(prepend "Io") &
-all_jobs+=("$!")
+injector_pid=$!
+all_jobs+=("$injector_pid")
 
 # Make sure injector starts properly.
 sleep 2
 
+#-------------------------------------------------------------------------------
 echo "$(now) Testing curl directly to the server."
-do_curl $origin_port $origin_addr:$origin_port $HTTP_OK
+do_curl "" $LOCAL_ORIGIN $HTTP_OK
 
+#-------------------------------------------------------------------------------
 echo "$(now) Testing curl to injector."
-do_curl $injector_tcp_port $origin_addr:$origin_port $HTTP_OK
+do_curl $INJECTOR_TCP_PORT $LOCAL_ORIGIN $HTTP_OK
 
+#-------------------------------------------------------------------------------
 echo "$(now) Starting injector helper."
 $unbuf ./injector_helper -i 127.0.0.1:7000 2> >(prepend "He") > >(prepend "Ho") &
 all_jobs+=("$!")
@@ -100,36 +106,33 @@ all_jobs+=("$!")
 # Wait for the injector helper to perform a test on the injector nedpoint.
 sleep 2
 
+#-------------------------------------------------------------------------------
 echo "$(now) Testing curl to injector_helper."
-r=0
 for i in 1 8 16; do
-    if ! test_n $i $origin_addr:$origin_port $HTTP_OK; then
-        r=1; break;
-    fi
+    test_n $i $LOCAL_ORIGIN $HTTP_OK || exit 1
 done
 
-if [ "$r" == "0" ]; then
-    echo "$(now) Testing HTTPS forwarding."
-    if ! do_curl $proxy_tcp_port https://google.com $HTTP_OK $HTTP_FOUND $HTTP_MOVED; then
-        r=2
-    fi
-fi
+#-------------------------------------------------------------------------------
+echo "$(now) Testing HTTPS forwarding."
+do_curl $HELPER_TCP_PORT https://google.com $HTTP_OK $HTTP_FOUND $HTTP_MOVED || exit 2
 
-if [ "$r" == "0" -a -n "$server_pid" ]; then
-    echo "$(now) Testing fast failure response."
-    # Kill the origin and try to connect to it. It shouldn't take long for the
-    # injector and proxy to find out the origin is unreachable.
-    kill -SIGINT $server_pid
+#-------------------------------------------------------------------------------
+echo "$(now) Testing fast failure response."
+# Kill the origin and try to connect to it. It shouldn't take long for the
+# injector and proxy to find out the origin is unreachable.
+kill -SIGINT $server_pid
 
-    start_time=$(seconds_since_epoch)
-    test_n 2 $origin_addr:$origin_port $HTTP_BAD_GATEWAY
-    end_time=$(seconds_since_epoch)
+start_time=$(seconds_since_epoch)
+test_n 2 $LOCAL_ORIGIN $HTTP_BAD_GATEWAY
+end_time=$(seconds_since_epoch)
+[ $(($end_time - $start_time)) -lt 5 ] || exit 3
 
-    if [ $(($end_time - $start_time)) -ge 5 ]; then
-        r=2
-    fi
-fi
+#-------------------------------------------------------------------------------
+echo "$(now) Testing response if injector is down."
+kill -SIGINT $injector_pid 2>/dev/null
+do_curl $HELPER_TCP_PORT $LOCAL_ORIGIN $HTTP_BAD_GATEWAY
 
+#-------------------------------------------------------------------------------
 echo "$(now) DONE"
 
 exit $r
