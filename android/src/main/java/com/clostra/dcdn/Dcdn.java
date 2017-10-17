a625bbc6423c0199c80c8cb4fe8c094913e7e7df
Greg Hazel <ghazel@gmail.com>
2017-10-17 02:47:09 -0700

    shutdown method

--- android/src/main/java/com/clostra/dcdn/Dcdn.java
+++ android/src/main/java/com/clostra/dcdn/Dcdn.java
@@ -3,11 +3,19 @@ package com.clostra.dcdn;
 public class Dcdn {
     static {
         System.loadLibrary("dcdn");
+    }
+
+    public static void init() {
         System.setProperty("http.proxyHost", "127.0.0.1");
         System.setProperty("https.proxyHost", "127.0.0.1");
         System.setProperty("http.proxyPort", "8006");
         System.setProperty("https.proxyPort", "8006");
     }
-    public static void init() {
+
+    public static void shutdown() {
+        System.clearProperty("http.proxyHost");
+        System.clearProperty("https.proxyHost");
+        System.clearProperty("http.proxyPort");
+        System.clearProperty("https.proxyPort");
     }
 }
