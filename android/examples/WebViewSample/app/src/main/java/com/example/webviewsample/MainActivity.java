736e21a3e8057590104faf3d9f3a7b8ee72ffd64
Greg Hazel <ghazel@gmail.com>
2017-10-17 18:15:23 -0700

    add address bar

--- android/examples/WebViewSample/app/src/main/java/com/example/webviewsample/MainActivity.java
+++ android/examples/WebViewSample/app/src/main/java/com/example/webviewsample/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.webviewsample;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.clostra.dcdn.Dcdn;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
+    private EditText mEditText;
 
     void load() {
-        mWebView.loadUrl("http://clostra.com");
+        String url = mEditText.getText().toString();
+        if (!url.isEmpty()) {
+            Uri u = Uri.parse(url);
+            if (Uri.parse(url).getScheme() == null) {
+                url = "http://" + url;
+                mEditText.setText(url, TextView.BufferType.EDITABLE);
+            }
+            mWebView.loadUrl(url);
+        }
+        InputMethodManager imm = (InputMethodManager) mEditText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+        mWebView.requestFocus();
     }
 
     @Override
@@ -22,16 +44,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Dcdn.init();
         setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.edit);
+        mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    load();
+                }
+                return false;
+            }
+        });
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                mEditText.setText(request.getUrl().toString(), TextView.BufferType.EDITABLE);
+                return false;
+            }
+        });
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        mEditText.setText("http://clostra.com", TextView.BufferType.EDITABLE);
         load();
     }
 
     @Override
     public void onBackPressed() {
-        if(mWebView.canGoBack()) {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
             super.onBackPressed();
