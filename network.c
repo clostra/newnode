89b793d78f3f102896a3ff748642c73fd91049c9
Greg Hazel <ghazel@gmail.com>
2017-11-07 15:21:42 -0800

    disable default content type

--- network.c
+++ network.c
@@ -233,6 +233,8 @@ network* network_setup(char *address, port_t port)
         fprintf(stderr, "evhttp_new failed\n");
         return NULL;
     }
+    // don't add any content type automatically
+    evhttp_set_default_content_type(http, NULL);
 
     debug("libevent method: %s\n", event_base_get_method(n->evbase));
 
