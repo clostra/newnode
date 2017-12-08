84dcd85ced6bcb3928ffd4786c9fe73e6c76e961
Greg Hazel <ghazel@gmail.com>
2017-12-07 17:52:12 -0800

    forward declare

--- network.h
+++ network.h
@@ -8,6 +8,9 @@
 #include <event2/http_struct.h>
 
 #include "utp.h"
+
+typedef struct network network;
+
 #include "dht.h"
 
 
@@ -21,14 +24,16 @@ typedef struct evdns_base evdns_base;
 typedef struct event event;
 typedef struct evhttp evhttp;
 typedef struct evbuffer evbuffer;
+typedef struct evutil_addrinfo evutil_addrinfo;
 typedef struct bufferevent bufferevent;
+typedef struct addrinfo addrinfo;
 typedef struct sockaddr sockaddr;
 typedef struct sockaddr_in sockaddr_in;
 typedef struct sockaddr_in6 sockaddr_in6;
 typedef struct sockaddr_storage sockaddr_storage;
 typedef in_port_t port_t;
 
-typedef struct {
+struct network {
     event_base *evbase;
     evdns_base *evdns;
     int fd;
@@ -36,7 +41,7 @@ typedef struct {
     utp_context *utp;
     dht *dht;
     evhttp *http;
-} network;
+};
 
 void evbuffer_clear(evbuffer *buf);
 network* network_setup(char *address, port_t port);
