fbe6c91374da7522a275f593e69d286a42454c36
Greg Hazel <ghazel@gmail.com>
2018-03-03 04:30:43 -0800

    silence log

--- dht.c
+++ dht.c
@@ -251,7 +251,7 @@ int dht_blacklisted(const sockaddr *sa, int salen)
         }
         socklen_t ss_len = blacklist[i]->ss_family == AF_INET6 ? sizeof(sockaddr_in6) : sizeof(sockaddr_in);
         if (memeq(sa, blacklist[i], ss_len)) {
-            debug("dht ignoring blacklisted node\n");
+            //debug("dht ignoring blacklisted node\n");
             return 1;
         }
     }
