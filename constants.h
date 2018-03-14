5445fa11a6fb96a66577b3949123e19b53c9198c
Greg Hazel <ghazel@gmail.com>
2018-03-14 11:31:26 -0700

    1.1.8

--- constants.h
+++ constants.h
@@ -1,7 +1,7 @@
 #ifndef __CONSTANTS_H__
 #define __CONSTANTS_H__
 
-#define VERSION "1.1.7"
+#define VERSION "1.1.8"
 
 #define injector_swarm "\xdfTH\xf4x\x17\x1bQcL\xe1\xebX\x18 \x05\x18]\x8c\x05"
 #define injector_proxy_swarm "4\xaa\xf4\x94\n,,1M\xc4\xaf\x03\xd4\xb5\xf5\xD\xee\x82-\x11"
