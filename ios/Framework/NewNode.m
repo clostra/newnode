11843d11fd17a1da8f7c514d837bc094d706d06f
Greg Hazel <ghazel@gmail.com>
2018-06-05 23:52:08 -0700

    add bugsnag

--- ios/Framework/NewNode.m
+++ ios/Framework/NewNode.m
@@ -1,10 +1,24 @@
 #include "newnode.h"
+#include "constants.h"
+#define BSG_KSLogger_Level TRACE
+#define BSG_LOG_LEVEL BSG_LOGLEVEL_DEBUG
 #include "NewNode-iOS.h"
+#import <BugsnagStatic/Bugsnag.h>
 
 @implementation NewNode
 
 + (void)initialize
 {
+    BugsnagConfiguration *config = BugsnagConfiguration.new;
+    config.apiKey = @"f5ce7103ec0ed93bee065269e8c2b676";
+    config.shouldAutoCaptureSessions = YES;
+    config.appVersion = @(VERSION);
+    /*
+    NSURLSessionConfiguration *urlConfig = NSURLSessionConfiguration.defaultSessionConfiguration;
+    urlConfig.connectionProxyDictionary = NewNode.connectionProxyDictionary;
+    config.session = [NSURLSession sessionWithConfiguration:urlConfig];
+    */
+    [Bugsnag startBugsnagWithConfiguration:config];
     newnode_init();
 }
 
