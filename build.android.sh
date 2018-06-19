ddd6c7fac840edfd6a7f1601850968c2c0970077
Greg Hazel <ghazel@gmail.com>
2018-06-18 19:46:28 -0700

    remove symbol hiding, fow now

--- build.android.sh
+++ build.android.sh
@@ -74,8 +74,8 @@ function build_android {
     FLAGS="-g -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Werror=shadow -Wfatal-errors \
       -fPIC -fblocks -fdata-sections -ffunction-sections \
       -fno-rtti -fno-exceptions -fno-common -fno-inline -fno-optimize-sibling-calls -funwind-tables -fno-omit-frame-pointer -fstack-protector-all \
-      -fvisibility=hidden -fvisibility-inlines-hidden -flto \
       -D__FAVOR_BSD -D_BSD_SOURCE -DANDROID"
+    #-fvisibility=hidden -fvisibility-inlines-hidden -flto \
     if [ ! -z "$DEBUG" ]; then
         FLAGS="$FLAGS -O0 -DDEBUG=1"
     else
