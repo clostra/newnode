50e32f836380f26d303e2d1c0f7a19973b0a2da2
Greg Hazel <ghazel@gmail.com>
2018-10-30 00:33:14 -0700

    calloc and assert

--- merkle_tree.c
+++ merkle_tree.c
@@ -22,9 +22,10 @@ bool merkle_tree_set_leaves(merkle_tree *m, const uint8_t *data, size_t length)
     if (length % member_sizeof(node, hash) != 0) {
         return false;
     }
+    _Static_assert(sizeof(node) == member_sizeof(node, hash), "node hash packing");
     m->leaves_num = length / member_sizeof(node, hash);
     m->nodes_alloc = m->leaves_num*2 - 1;
-    m->nodes = alloc(m->nodes_alloc * sizeof(node));
+    m->nodes = calloc(m->nodes_alloc, sizeof(node));
     memcpy(m->nodes, data, length);
     return true;
 }
