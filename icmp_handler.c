8dfa2dfde169db4cee38b6eca214e909fb767018
Greg Hazel <ghazel@gmail.com>
2020-01-30 15:06:16 -0800

    icmp6

--- icmp_handler.c
+++ icmp_handler.c
@@ -24,20 +24,20 @@ typedef struct sock_extended_err sock_extended_err;
 void icmp_handler(network *n)
 {
     for (;;) {
-        unsigned char vec_buf[4096];
-        unsigned char ancillary_buf[4096];
+        uint8_t vec_buf[4096];
+        uint8_t ancillary_buf[4096];
         iovec iov = { vec_buf, sizeof(vec_buf) };
-        sockaddr_in remote;
+        sockaddr_storage remote;
 
-        msghdr msg;
-        memset(&msg, 0, sizeof(msg));
-        msg.msg_name = &remote;
-        msg.msg_namelen = sizeof(remote);
-        msg.msg_iov = &iov;
-        msg.msg_iovlen = 1;
-        msg.msg_flags = 0;
-        msg.msg_control = ancillary_buf;
-        msg.msg_controllen = sizeof(ancillary_buf);
+        msghdr msg = {
+            .msg_name = (sockaddr *)&remote,
+            .msg_namelen = sizeof(remote),
+            .msg_iov = &iov,
+            .msg_iovlen = 1,
+            .msg_flags = 0,
+            .msg_control = ancillary_buf,
+            .msg_controllen = sizeof(ancillary_buf)
+        };
 
         ssize_t len = recvmsg(n->fd, &msg, MSG_ERRQUEUE | MSG_DONTWAIT);
 
@@ -48,28 +48,30 @@ void icmp_handler(network *n)
             pdie("recvmsg");
         }
 
+        socklen_t remote_len = sockaddr_get_length((const sockaddr *)&remote);
+
         time_t tosleep;
-        dht_process_icmp(n->dht, (const byte*) &msg, sizeof(msg), (sockaddr *)&remote, sizeof(remote), &tosleep);
+        dht_process_icmp(n->dht, (const byte*)&msg, sizeof(msg), (const sockaddr *)&remote, remote_len, &tosleep);
 
         for (cmsghdr *cmsg = CMSG_FIRSTHDR(&msg); cmsg; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
-            if (cmsg->cmsg_type != IP_RECVERR) {
-                debug("Unhandled errqueue type: %d\n", cmsg->cmsg_type);
-                continue;
-            }
-
             if (cmsg->cmsg_level != SOL_IP) {
                 debug("Unhandled errqueue level: %d\n", cmsg->cmsg_level);
                 continue;
             }
 
-            ddebug("errqueue: IP_RECVERR, SOL_IP, len %zd\n", cmsg->cmsg_len);
-
-            if (remote.sin_family != AF_INET) {
-                debug("Address family is %d, not AF_INET?  Ignoring\n", remote.sin_family);
+            if (cmsg->cmsg_type == IP_RECVERR) {
+                ddebug("errqueue: IP_RECVERR, SOL_IP, len %zd\n", cmsg->cmsg_len);
+            } else if (cmsg->cmsg_type == IPV6_RECVERR) {
+                ddebug("errqueue: IPV6_RECVERR, SOL_IP, len %zd\n", cmsg->cmsg_len);
+            } else {
+                debug("Unhandled errqueue type: %d\n", cmsg->cmsg_type);
                 continue;
             }
 
-            ddebug("Remote host: %s:%d\n", inet_ntoa(remote.sin_addr), ntohs(remote.sin_port));
+            char host[NI_MAXHOST];
+            char serv[NI_MAXSERV];
+            getnameinfo((const sockaddr *)&remote, remote_len, host, sizeof(host), serv, sizeof(serv), NI_NUMERICHOST|NI_NUMERICSERV);
+            ddebug("Remote host: %s:%s\n", host, serv);
 
             sock_extended_err *e = (sock_extended_err *)CMSG_DATA(cmsg);
 
@@ -78,7 +80,7 @@ void icmp_handler(network *n)
                 continue;
             }
 
-            if (e->ee_origin != SO_EE_ORIGIN_ICMP) {
+            if (e->ee_origin != SO_EE_ORIGIN_ICMP && e->ee_origin != SO_EE_ORIGIN_ICMP6) {
                 debug("errqueue: Unexpected origin: %d\n", e->ee_origin);
                 continue;
             }
@@ -88,18 +90,6 @@ void icmp_handler(network *n)
 
             // "Node that caused the error"
             // "Node that generated the error"
-            sockaddr *icmp_addr = (sockaddr *)SO_EE_OFFENDER(e);
-            sockaddr_in *icmp_sin = (sockaddr_in *)icmp_addr;
-
-            if (icmp_addr->sa_family != AF_INET) {
-                debug("ICMP's address family is %d, not AF_INET?\n", icmp_addr->sa_family);
-                continue;
-            }
-
-            if (icmp_sin->sin_port != 0) {
-                debug("ICMP's 'port' is not 0?\n");
-                continue;
-            }
 
             ddebug("msg_flags: %d", msg.msg_flags);
             if (o_debug >= 2) {
@@ -122,10 +112,10 @@ void icmp_handler(network *n)
 
             if (e->ee_type == 3 && e->ee_code == 4) {
                 ddebug("ICMP type 3, code 4: Fragmentation error, discovered MTU %d\n", e->ee_info);
-                utp_process_icmp_fragmentation(n->utp, vec_buf, len, (sockaddr *)&remote, sizeof(remote), e->ee_info);
+                utp_process_icmp_fragmentation(n->utp, vec_buf, len, (const sockaddr *)&remote, remote_len, e->ee_info);
             } else {
                 ddebug("ICMP type %d, code %d\n", e->ee_type, e->ee_code);
-                utp_process_icmp_error(n->utp, vec_buf, len, (sockaddr *)&remote, sizeof(remote));
+                utp_process_icmp_error(n->utp, vec_buf, len, (const sockaddr *)&remote, remote_len);
             }
         }
     }
