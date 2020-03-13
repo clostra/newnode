4df1e367a02bdecba6abe688769267fba338ed67
Greg Hazel <ghazel@gmail.com>
2020-03-13 14:58:14 -0700

    request location permission

---
+++ android/src/main/java/com/clostra/newnode/internal/PermissionActivity.java
@@ -0,0 +1,41 @@
+package com.clostra.newnode.internal;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class PermissionActivity extends Activity {
+    static final String TAG = "PermissionActivity";
+
+    private static final int PERMISSIONS_REQUEST_CODE = 1001;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+        case PERMISSIONS_REQUEST_CODE:
+            if  (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Log.e(TAG, "Permission is not granted!");
+            } else {
+                Log.e(TAG, "Permission granted!");
+            }
+            finish();
+            break;
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate permission activity starting..");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_CODE);
+        } else {
+            finish();
+        }
+    }
+}
