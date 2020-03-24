cb012552a16c90d855d21f1ad04fad0de5743671
Greg Hazel <ghazel@gmail.com>
2020-03-23 20:36:13 -0700

    d2d.c

--- build.ios.sh
+++ build.ios.sh
@@ -55,7 +55,7 @@ function build_ios {
     rm -rf $TRIPLE || true
     rm *.o || true
     clang $CFLAGS -c dht/dht.c -o dht_dht.o
-    for file in bev_splice.c base64.c client.c dht.c http.c log.c lsd.c \
+    for file in bev_splice.c base64.c client.c dht.c d2d.c http.c log.c lsd.c \
                 icmp_handler.c hash_table.c merkle_tree.c network.c \
                 obfoo.c sha1.c timer.c thread.c utp_bufferevent.c; do
         clang $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS $LIBBUGSNAG_CFLAGS -c $file
