adaadee95693f4ab266224605f71b161cbdc3fc1
Greg Hazel <ghazel@gmail.com>
2020-05-18 16:33:06 -0700

    iOS VPN

---
+++ ios/NewNode VPN/Tunnel/NewNode-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  NewNode-Bridging-Header.h
+//  NewNode VPN
+//
+//  Created by Gregory Hazel on 7/9/19.
+//  Copyright © 2019 Clostra. All rights reserved.
+//
+
+#ifndef NewNode_Bridging_Header_h
+#define NewNode_Bridging_Header_h
+
+#include "newnode.h"
+#include "constants.h"
+#include "log.h"
+
+#endif /* NewNode_Bridging_Header_h */
