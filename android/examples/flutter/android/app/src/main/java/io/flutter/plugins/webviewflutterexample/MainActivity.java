44ef907ddd79c08f789b57460045b2826ca7ca7e
Greg Hazel <ghazel@gmail.com>
2020-06-06 16:15:49 -0700

    integrate newnode

--- android/examples/flutter/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/MainActivity.java
+++ android/examples/flutter/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/MainActivity.java
@@ -2,10 +2,12 @@ package io.flutter.plugins.webviewflutterexample;
 
 import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
+import com.clostra.newnode.NewNode;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    NewNode.init();
   }
 }
