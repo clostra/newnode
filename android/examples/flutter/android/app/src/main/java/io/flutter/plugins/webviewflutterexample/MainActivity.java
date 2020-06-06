7c3349da81ec0688e52feafc4b112fd5b9c5f28c
Greg Hazel <ghazel@gmail.com>
2020-06-06 16:14:18 -0700

    use v2 embedding

---
+++ android/examples/flutter/android/app/src/main/java/io/flutter/plugins/webviewflutterexample/MainActivity.java
@@ -0,0 +1,11 @@
+package io.flutter.plugins.webviewflutterexample;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
