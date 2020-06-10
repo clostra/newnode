3b9714e49212a4e1f8ea93498d7009a9e8a5e8fd
Greg Hazel <ghazel@gmail.com>
2020-06-10 00:32:37 -0700

    switch size, label, and behavior

--- android/vpn/app/src/main/java/com/clostra/newnode/vpn/VpnActivity.java
+++ android/vpn/app/src/main/java/com/clostra/newnode/vpn/VpnActivity.java
@@ -1,41 +1,70 @@
 package com.clostra.newnode.vpn;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class VpnActivity extends Activity {
+
+    public final static String ACTION_STATE = "com.clostra.newnode.vpn.STATE";
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            TextView t = findViewById(R.id.textView);
+            t.setText(intent.getIntExtra("state", 0));
+        }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.form);
+        setContentView(R.layout.main);
 
-        findViewById(R.id.connect).setOnClickListener(v -> {
-            SharedPreferences prefs = getSharedPreferences("vpn", MODE_PRIVATE);
-            if (prefs.getBoolean("enabled", false)) {
-                prefs.edit()
-                    .putBoolean("enabled", false)
-                    .apply();
-                startService(getServiceIntent().setAction(VpnService.ACTION_DISCONNECT));
-                return;
-            }
+        registerReceiver(receiver, new IntentFilter(ACTION_STATE));
+
+        setVpnState();
+        SharedPreferences prefs = getSharedPreferences("vpn", MODE_PRIVATE);
+        Switch switch1 = findViewById(R.id.switch1);
+        switch1.setChecked(prefs.getBoolean("enabled", false));
+        switch1.setOnCheckedChangeListener((view, isChecked) -> {
             prefs.edit()
-                .putBoolean("enabled", true)
-                .apply();
-            Intent intent = VpnService.prepare(VpnActivity.this);
-            if (intent != null) {
-                startActivityForResult(intent, 0);
-            } else {
-                onActivityResult(0, RESULT_OK, null);
-            }
+                    .putBoolean("enabled", isChecked)
+                    .apply();
+            setVpnState();
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 
     @Override
     protected void onActivityResult(int request, int result, Intent data) {
         if (result == RESULT_OK) {
+            sendBroadcast(new Intent(VpnActivity.ACTION_STATE).putExtra("state", R.string.connecting));
             startService(getServiceIntent().setAction(VpnService.ACTION_CONNECT));
+        }
+    }
+
+    private void setVpnState() {
+        SharedPreferences prefs = getSharedPreferences("vpn", MODE_PRIVATE);
+        if (!prefs.getBoolean("enabled", false)) {
+            startService(getServiceIntent().setAction(VpnService.ACTION_DISCONNECT));
+            return;
+        }
+        Intent intent = VpnService.prepare(VpnActivity.this);
+        if (intent != null) {
+            startActivityForResult(intent, 0);
+        } else {
+            onActivityResult(0, RESULT_OK, null);
         }
     }
 
