4f7c4b18da5ba8a3b8da411872376db5e9f14854
Greg Hazel <ghazel@gmail.com>
2020-08-12 01:09:09 -0700

    new bugsnag api

--- ios/Framework/NewNode.m
+++ ios/Framework/NewNode.m
@@ -17,14 +17,13 @@ NetService *ns = nil;
 {
     BugsnagConfiguration *config = BugsnagConfiguration.new;
     config.apiKey = @"f5ce7103ec0ed93bee065269e8c2b676";
-    config.shouldAutoCaptureSessions = YES;
     config.appVersion = @(VERSION);
     /*
     NSURLSessionConfiguration *urlConfig = NSURLSessionConfiguration.defaultSessionConfiguration;
     urlConfig.connectionProxyDictionary = NewNode.connectionProxyDictionary;
     config.session = [NSURLSession sessionWithConfiguration:urlConfig];
     */
-    [Bugsnag startBugsnagWithConfiguration:config];
+    [Bugsnag startWithConfiguration:config];
 
     NSString *cachesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).lastObject;
     chdir(cachesPath.UTF8String);
