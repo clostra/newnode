213f3c5d0f0df661e4ab454d0107b74084cfdf31
Greg Hazel <ghazel@gmail.com>
2020-08-27 12:46:13 -0700

    more icmp changes

--- dht.h
+++ dht.h
@@ -13,7 +13,7 @@ typedef struct sockaddr sockaddr;
 dht* dht_setup(network *n);
 time_t dht_tick(dht *d);
 bool dht_process_udp(dht *d, const uint8_t *buffer, size_t len, const sockaddr *to, socklen_t tolen, time_t *tosleep);
-bool dht_process_icmp(dht *d, const uint8_t *buffer, size_t len, const sockaddr *to, socklen_t tolen, time_t *tosleep);
+bool dht_process_icmp_error(dht *d, const uint8_t *buffer, size_t len, const sockaddr *to, socklen_t tolen);
 void dht_announce(dht *d, const uint8_t *info_hash);
 void dht_get_peers(dht *d, const uint8_t *info_hash);
 size_t dht_num_searches(void);
