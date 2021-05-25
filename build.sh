f7240215e8a59678e6cef8a6e3605c8c4b7af941
Greg Hazel <ghazel@gmail.com>
2021-05-25 11:21:31 -0700

    s/Libevent/libevent/

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ if [ ! -d native ]; then
     make install
 fi
 cd ..
-libevent_CFLAGS=-Ilibevent/native/include
-libevent="libevent/native/lib/libevent.a libevent/native/lib/libevent_pthreads.a"
+LIBEVENT_CFLAGS=-Ilibevent/native/include
+LIBEVENT="libevent/native/lib/libevent.a libevent/native/lib/libevent_pthreads.a"
 
 
 cd libsodium
@@ -79,13 +79,13 @@ rm *.o || true
 clang $CFLAGS -c dht/dht.c -o dht_dht.o
 for file in backtrace.c client.c client_main.c d2d.c injector.c dht.c bev_splice.c base64.c http.c log.c lsd.c icmp_handler.c hash_table.c \
             merkle_tree.c network.c obfoo.c sha1.c stall_detector.c timer.c thread.c utp_bufferevent.c; do
-    clang $CFLAGS $LIBUTP_CFLAGS $libevent_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -c $file
+    clang $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -c $file
 done
 
 mv client.o client.o.tmp
 mv client_main.o client_main.o.tmp
-clang $CFLAGS -o injector *.o $LRT $LM $LIBUTP $libevent $LIBSODIUM $LIBBLOCKSRUNTIME -lpthread
+clang $CFLAGS -o injector *.o $LRT $LM $LIBUTP $LIBEVENT $LIBSODIUM $LIBBLOCKSRUNTIME -lpthread
 mv injector.o injector.o.tmp
 mv client.o.tmp client.o
 mv client_main.o.tmp client_main.o
-clang $CFLAGS -o client *.o $LRT $LM $LIBUTP $libevent $LIBSODIUM $LIBBLOCKSRUNTIME -lpthread
+clang $CFLAGS -o client *.o $LRT $LM $LIBUTP $LIBEVENT $LIBSODIUM $LIBBLOCKSRUNTIME -lpthread
