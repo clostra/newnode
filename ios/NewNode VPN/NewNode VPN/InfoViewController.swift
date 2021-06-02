0017c740bb972f678d172e070ba22ff0d7a5e3ea
Mikhail Koroteev <horis.mkoroteev@gmail.com>
2021-06-03 00:53:07 +0300

    Implement new UI. Extract resource strings and colors. Remove LLSwitch. Refactoring ViewController.

---
+++ ios/NewNode VPN/NewNode VPN/InfoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  InfoViewController.swift
+//  NewNode VPN
+//
+//  Created by Mikhail Koroteev on 05/31/21.
+//  Copyright © 2021 Clostra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoViewController: UIViewController {
+    @IBOutlet weak var infoHeader: UITextView!
+    @IBOutlet weak var infoText: UITextView!
+    
+    @IBAction func tapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        infoText.text = NSLocalizedString("info_text", comment: "")
+        infoHeader.text = NSLocalizedString("info_title", comment: "")
+        infoHeader.adjustsFontForContentSizeCategory = true
+        
+        if UIScreen.main.nativeBounds.height < 1200 {
+            infoText.font = UIFont.systemFont(ofSize: 14.0)
+            infoHeader.font = UIFont.systemFont(ofSize: 22.0)
+        }
+    }
+}
