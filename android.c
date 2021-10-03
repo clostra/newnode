8dfb3faa9981d28bb3c05ab327e57e3d0e9c2787
Greg Hazel <ghazel@gmail.com>
2021-10-02 17:02:31 -0700

    fix buf type

--- android.c
+++ android.c
@@ -350,7 +350,7 @@ JNIEXPORT void JNICALL Java_com_clostra_newnode_internal_NewNode_packetReceived(
     });
 }
 
-ssize_t d2d_sendto(const uint8* buf, size_t len, const sockaddr_in6 *sin6)
+ssize_t d2d_sendto(const uint8_t* buf, size_t len, const sockaddr_in6 *sin6)
 {
     JNIEnv *env = get_env();
     push_frame();
