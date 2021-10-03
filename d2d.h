51dc146412dba643e0892ade87fac97bc4122efd
Greg Hazel <ghazel@gmail.com>
2021-10-02 17:12:02 -0700

    fix buf type

--- d2d.h
+++ d2d.h
@@ -1,7 +1,7 @@
 #ifndef __D2D_H__
 #define __D2D_H__
 
-ssize_t d2d_sendto(const uint8* buf, size_t len, const sockaddr_in6 *sin6);
+ssize_t d2d_sendto(const uint8_t* buf, size_t len, const sockaddr_in6 *sin6);
 sockaddr_in6 endpoint_to_addr(const uint8_t *endpoint, size_t len);
 const uint8_t* addr_to_endpoint(const sockaddr_in6 *sin6);
 
