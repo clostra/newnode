37c612f3e0ad2b1d7edf13dc4622392224e2eae2
Greg Hazel <ghazel@gmail.com>
2021-10-14 22:41:22 -0700

    use IN6_IS_ADDR_UNIQUE_LOCAL

--- d2d.c
+++ d2d.c
@@ -4,38 +4,42 @@
 #include "network.h"
 
 
-#ifndef ANDROID
-ssize_t d2d_sendto(const uint8* buf, size_t len, const sockaddr_in6 *sin6)
+#ifndf ANDROID
+ssize_t d2d_sendto(const uint8_t* buf, size_t len, const sockaddr_in6 *sin6)
 {
     return -1;
 }
 #endif
 
-#define LINKLOCAL_PREFIX_LENGTH 2
+#define UNIQUE_LOCAL_PREFIX_LENGTH 1
+
+typedef struct {
+    uint8_t addr[sizeof(in6_addr) - UNIQUE_LOCAL_PREFIX_LENGTH];
+    port_t port;
+} PACKED unique_local;
 
 sockaddr_in6 endpoint_to_addr(const uint8_t *endpoint, size_t len)
 {
+    assert(len <= sizeof(unique_local));
+    unique_local u = {0};
+    memcpy(&u, endpoint, len);
+
     sockaddr_in6 sin6 = {
         .sin6_family = AF_INET6,
-        // link-local unicast
-        .sin6_addr.s6_addr[0] = 0xfe,
-        .sin6_addr.s6_addr[1] = 0x80
+        //  Unique Local IPv6 Unicast Address
+        .sin6_addr.s6_addr[0] = 0xfc,
+        .sin6_port = u.port
     };
-    const size_t addrmax = sizeof(sin6.sin6_addr.s6_addr) - LINKLOCAL_PREFIX_LENGTH;
-    assert(IN6_IS_ADDR_LINKLOCAL(&sin6.sin6_addr));
-    memcpy(&sin6.sin6_addr.s6_addr[LINKLOCAL_PREFIX_LENGTH], endpoint, MIN(len, addrmax));
-    if (len > addrmax) {
-        memcpy(&sin6.sin6_port, &endpoint[addrmax], MIN(len - addrmax, sizeof(port_t)));
-        assert(len <= addrmax + sizeof(port_t));
-    }
+    memcpy(&sin6.sin6_addr.s6_addr[UNIQUE_LOCAL_PREFIX_LENGTH], u.addr, sizeof(u.addr));
+    assert(IN6_IS_ADDR_UNIQUE_LOCAL(&sin6.sin6_addr));
+
     return sin6;
 }
 
 const uint8_t* addr_to_endpoint(const sockaddr_in6 *sin6)
 {
-    const size_t addrlen = sizeof(sin6->sin6_addr.s6_addr);
-    static uint8_t e[addrlen] = {0};
-    memcpy(e, &sin6->sin6_addr.s6_addr[LINKLOCAL_PREFIX_LENGTH], addrlen - LINKLOCAL_PREFIX_LENGTH);
-    memcpy(&e[addrlen - LINKLOCAL_PREFIX_LENGTH], &sin6->sin6_port, sizeof(sin6->sin6_port));
-    return e;
+    static unique_local u;
+    memcpy(u.addr, &sin6->sin6_addr.s6_addr[UNIQUE_LOCAL_PREFIX_LENGTH], sizeof(u.addr));
+    u.port = sin6->sin6_port;
+    return u.addr;
 }
