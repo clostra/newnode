ee51c00fe6c916380727335d29a435d6420ea2c4
Greg Hazel <ghazel@gmail.com>
2022-01-20 00:23:57 -0800

    location disclosure

--- android/src/main/java/com/clostra/newnode/internal/PermissionActivity.java
+++ android/src/main/java/com/clostra/newnode/internal/PermissionActivity.java
@@ -2,8 +2,10 @@ package com.clostra.newnode.internal;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -48,17 +50,43 @@ public class PermissionActivity extends Activity {
             Manifest.permission.BLUETOOTH_CONNECT,
             Manifest.permission.BLUETOOTH_SCAN,
         };
+        boolean locationDisclosure = false;
         List<String> permissionsToRequest = new ArrayList<>();
         for (String permission : permissions) {
             if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 Log.d(TAG, "requesting " + permission);
                 permissionsToRequest.add(permission);
+                if (permission == Manifest.permission.ACCESS_COARSE_LOCATION ||
+                    permission == Manifest.permission.ACCESS_FINE_LOCATION) {
+                    locationDisclosure = true;
+                }
             }
         }
-        if (permissionsToRequest.size() > 0) {
-            ActivityCompat.requestPermissions(this,
-                permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
-                PERMISSIONS_REQUEST_CODE);
+        if (permissionsToRequest.size() == 0) {
+            return;
+        }
+        if (locationDisclosure) {
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        ActivityCompat.requestPermissions(PermissionActivity.this,
+                            permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                            PERMISSIONS_REQUEST_CODE);
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                    }
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("This app uses current location to find nearby connections  in the background")
+                .setPositiveButton(android.R.string.ok, dialogClickListener)
+                .setNegativeButton(android.R.string.cancel, dialogClickListener).show();
         }
     }
 }
