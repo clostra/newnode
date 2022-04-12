858c2ba5f7857bdee8dc3a19a3d507a3ed2b8aef
Greg Hazel <ghazel@gmail.com>
2022-04-12 11:58:45 -0700

    move parson flags, refactor some

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ export CC=clang
 export CXX=clang++
 
 
+PARSON_CFLAGS="-Iparson"
+
 cd libevent
 if [ ! -d native ]; then
     ./autogen.sh
@@ -57,7 +59,7 @@ fi
 FLAGS="-g -Werror -Wall -Wextra -Wno-deprecated-declarations -Wno-unused-parameter -Wno-unused-variable -Wno-error=shadow -Wfatal-errors \
   -fPIC -fblocks -fdata-sections -ffunction-sections \
   -fno-rtti -fno-exceptions -fno-common -fno-inline -fno-optimize-sibling-calls -funwind-tables -fno-omit-frame-pointer -fstack-protector-all \
-  -D__FAVOR_BSD -D_BSD_SOURCE -D_DEFAULT_SOURCE -Iparson"
+  -D__FAVOR_BSD -D_BSD_SOURCE -D_DEFAULT_SOURCE"
 # -fvisibility=hidden -fvisibility-inlines-hidden -flto=thin \
 if [ ! -z ${DEBUG+x} ]; then
     FLAGS="$FLAGS -O0 -DDEBUG=1 -fsanitize=address -fsanitize=undefined --coverage"
@@ -75,27 +77,30 @@ echo "int main() {}"|clang -x c - -lrt 2>/dev/null && LRT="-lrt"
 LM=
 echo -e "#include <math.h>\nint main() { log(2); }"|clang -x c - 2>/dev/null || LM="-lm"
 
+CDEPS="$PARSON_CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS"
+CLIBS="$LRT $LM $LIBUTP $LIBEVENT $LIBSODIUM $LIBBLOCKSRUNTIME"
+
 rm -f *.o || true
 clang $CFLAGS -c dht/dht.c -o dht_dht.o
 clang $CFLAGS -c parson/parson.c -o parson.o
 for file in backtrace.c client.c d2d.c injector.c dht.c bev_splice.c base64.c http.c log.c lsd.c icmp_handler.c hash_table.c \
-            merkle_tree.c network.c obfoo.c sha1.c stall_detector.c timer.c thread.c utp_bufferevent.c; do
-    clang $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -c $file
+            merkle_tree.c network.c obfoo.c sha1.c stall_detector.c timer.c thread.c utp_bufferevent.c bufferevent_utp.c; do
+    clang $CFLAGS $CDEPS -c $file
 done
 
 mv client.o client.o.tmp
-clang $CFLAGS -o injector *.o $LRT $LM $LIBUTP $LIBEVENT $LIBSODIUM $LIBBLOCKSRUNTIME -lpthread
+clang $CFLAGS -o injector *.o $CLIBS -lpthread
 mv injector.o injector.o.tmp
 mv client.o.tmp client.o
-clang $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -c dns_prefetch.c
+clang $CFLAGS $CDEPS -c dns_prefetch.c
 case $(uname -s):$(uname -m) in
     Darwin:*) 
-        clang $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -c dns_prefetch_macos.c
-        clang -framework Foundation $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -I. -o client client_main.c ios/HTTPSRequest.m *.o $LRT $LM $LIBUTP $LIBEVENT $LIBSODIUM $LIBBLOCKSRUNTIME -lpthread
+        clang $CFLAGS $CDEPS -c dns_prefetch_macos.c
+        clang -framework Foundation $CDEPS -I. -o client client_main.c ios/HTTPSRequest.m *.o $CLIBS -lpthread
         ;;
     *)
-        clang $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -c https_wget.c
-        clang $CFLAGS $LIBUTP_CFLAGS $LIBEVENT_CFLAGS $LIBSODIUM_CFLAGS $LIBBLOCKSRUNTIME_CFLAGS -c client_main.c
-        clang $CFLAGS -o client *.o $LRT $LM $LIBUTP $LIBEVENT $LIBSODIUM $LIBBLOCKSRUNTIME -lpthread
+        clang $CFLAGS $CDEPS -c https_wget.c
+        clang $CFLAGS $CDEPS -c client_main.c
+        clang $CFLAGS -o client *.o $CLIBS -lpthread
         ;;
 esac
