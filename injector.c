bd22eea287a5862c9d453d17c4e9258825c41d4e
Greg Hazel <ghazel@gmail.com>
2022-04-22 15:16:23 -0700

    do nothing on cancel

--- injector.c
+++ injector.c
@@ -222,7 +222,11 @@ void error_cb(evhttp_request_error error, void *arg)
 {
     proxy_request *p = (proxy_request*)arg;
     debug("p:%p (%.2fms) %s %d\n", p, pdelta(p), __func__, error);
+    assert(p->req);
     p->req = NULL;
+    if (error == EVREQ_HTTP_REQUEST_CANCEL) {
+        return;
+    }
     if (p->server_req) {
         if (p->server_req->evcon) {
             evhttp_connection_set_closecb(p->server_req->evcon, NULL, NULL);
